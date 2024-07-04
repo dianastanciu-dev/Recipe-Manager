fe831d593eed47ecc4c9017ea588e9e70633f7c5
Lara DianaS <lara@Laras-MacBook-Air.local>
2024-07-04 15:18:47 +0300

    hanging in..

--- src/main/java/com/example/recipemanager/repository/RecipeRepo.java
+++ src/main/java/com/example/recipemanager/repository/RecipeRepo.java
@@ -3,7 +3,9 @@ package com.example.recipemanager.repository;
 import com.example.recipemanager.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepo extends JpaRepository<Recipe, Long> {
     //Long is the type of the primary key --> Long id
-
+    Optional<Recipe> findRecipeById(Long id);
 }
--- src/main/java/com/example/recipemanager/service/RecipeService.java
+++ src/main/java/com/example/recipemanager/service/RecipeService.java
@@ -1,10 +1,12 @@
 package com.example.recipemanager.service;
 
+import com.example.recipemanager.exception.UserNotFoundException;
 import com.example.recipemanager.model.Recipe;
 import com.example.recipemanager.repository.RecipeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,4 +23,24 @@ public class RecipeService {
         recipe.setRecipeCode(UUID.randomUUID().toString());
         return recipeRepo.save(recipe);
     }
+
+    public List<Recipe> findAllRecipes() {
+        return recipeRepo.findAll();
+    }
+
+    public Recipe updateRecipe(Recipe recipe)
+    {
+        return recipeRepo.save(recipe);
+    }
+
+    public Recipe findRecipeById(Long id)
+    {
+        return recipeRepo.findRecipeById(id).orElseThrow(() -> new UserNotFoundException("Recipe with id "+id+" not found "));
+    }
+
+    public void deleteRecipe(Long id){
+        recipeRepo.deleteById(id);
+    }
+
+
 }
